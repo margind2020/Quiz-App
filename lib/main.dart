2f3bff9c510d2740185877cd5b18cb7f1a7e0efc
dhami margin nitinbhai <margin.sdjic@gmail.com>
2025-04-17 19:43:25 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import "package:flutter/material.dart";
+import 'package:flutter_application_1/startscreen.dart';
+void main(){
+  runApp(MaterialApp(home: Scaffold(backgroundColor: Colors.blue,
+    body: Startscreen()
+  ),));
+}
---
+++ lib/startscreen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+
+class Startscreen extends StatelessWidget{
+  const Startscreen({super.key});
+  @override
+  Widget build(context){
+    return Center(child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset("assets/images/quiz_logo.png",
+        width: 250,),
+        const SizedBox(height: 50,),
+        const Text("Learn Flutter the fun way",
+        style: TextStyle(color: Colors.white,
+        fontSize: 25
+        ),
+        ),
+        const SizedBox(height: 20,),
+        OutlinedButton.icon(onPressed: (){}, 
+        label: Text("Start Quiz",
+        style: TextStyle(color:Colors.white,
+        fontSize: 15),
+        ),icon: Icon(Icons.arrow_right_alt,
+        color: Colors.white,size: 25,),
+        )
+      ],
+    ),
+    );
+  }
+  }
